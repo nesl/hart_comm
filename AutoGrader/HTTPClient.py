e1deb1fe66414c802bc78e49ef3b44f22d8103c2
Bo-Jhang Ho <timestringalpha@gmail.com>
2017-09-30 17:15:41 -0700

    Add the capability to disable SSL verification

--- AutoGrader/HTTPClient.py
+++ AutoGrader/HTTPClient.py
@@ -7,6 +7,8 @@ class HTTPClient(object):
     def __init__(self, config, server_listening_port):
         remote_host = config["remotehost"]
         http_s = 's' if 'use_https' in config and config['use_https'] else ''
+        self.ssl_verify = False if 'ssl_verify' in config and not config['ssl_verify'] else True
+        
         port_prefix = (':%d' % config["remoteport"]) if 'remoteport' in config else ''
         self.remote_http = 'http%s://%s%s' % (http_s, remote_host, port_prefix)
         self.report_listening_port = server_listening_port
@@ -16,11 +18,16 @@ class HTTPClient(object):
             r = requests.post(self.remote_http + '/tb/send-summary/',
                     data={'localport': self.report_listening_port, 'testbed_type': testbed_type},
                     headers={'content-type': "application/x-www-form-urlencoded"},
-                    timeout=1.0)
+                    timeout=1.0, verify=self.ssl_verify)
             if r.status_code != 200:
                 print('[Request Error] Server rejected the request')
                 return False
         except:
+            #TODO: delete these
+            import traceback
+            import sys
+            exc_info = sys.exc_info()
+            traceback.print_exception(*exc_info)
             print('[Network Error] Remote server is down... Failed on sending summary messages')
             return False
         return True
@@ -30,7 +37,7 @@ class HTTPClient(object):
             r = requests.post(self.remote_http + '/tb/send-status/',
                     data={'localport': self.report_listening_port, 'status': status},
                     headers={'content-type': "application/x-www-form-urlencoded"},
-                    timeout=1.0)
+                    timeout=1.0, verify=self.ssl_verify)
             if r.status_code != 200:
                 print('[Request Error] Server rejected the request')
                 return False
@@ -49,7 +56,7 @@ class HTTPClient(object):
                         field_name, open(output_file_dict[file_name], 'rb'), 'text/plain')
             # The files can be big, wait longer time to transfer data back
             r = requests.post(self.remote_http + '/tb/send-dut-output/',
-                    data=data, files=files, timeout=10.0)
+                    data=data, files=files, timeout=10.0, verify=self.ssl_verify)
             if r.status_code != 200:
                 print('[Request Error] Server rejected the request')
                 return False
