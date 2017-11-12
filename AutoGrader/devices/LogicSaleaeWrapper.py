c7f2270f9eeae2c6bce7d09ba6cf30ff610630ba
Bo-Jhang Ho <timestringalpha@gmail.com>
2017-11-11 19:13:11 -0800

    Finalize LogicSaleaeWrapper

--- AutoGrader/devices/LogicSaleaeWrapper.py
+++ AutoGrader/devices/LogicSaleaeWrapper.py
@@ -1,23 +1,37 @@
 import sys
 import traceback
 import shutil
+import os
+import time
+
 import saleae
 
 from AutoGrader.devices import HardwareBase
-
+from AutoGrader.devices.fileio.logic_saleae_waveform_file_writer import LogicSaleaeWaveformFileWriter
 
 class LogicSaleaeWrapper(HardwareBase):
-    # parameters
-    output_waveform_path = None
+    """
+    Before testbed is running, make sure start Logic Saleae GUI first
+    """
 
-    # device info
-    name = None
-    config = None
-
-    # Saleae instance
-    saleae_dev = None
+    TMP_OUTPUT_PATH = '/tmp/logic.csv'
 
     def __init__(self, name, config, hardware_engine, file_folder):
+        
+        # parameters
+        self.output_waveform_path = None
+
+        # device info
+        self.name = None
+        self.config = None
+
+        # Saleae instance
+        self.saleae_dev = None
+
+        # execution info
+        self.execution_start_time = None
+
+        # parse config
         if "output_waveform_file" not in config:
             raise Exception('"output_waveform_file" field is required')
         self.output_waveform_path = os.path.join(file_folder, config['output_waveform_file'])
@@ -32,16 +46,30 @@ class LogicSaleaeWrapper(HardwareBase):
         self.name = name
         self.config = config
 
-    def on_before_execution(self, deadline_sec):
-        self.seleae_dev.set_capture_seconds(deadline_sec)
+    def on_before_execution(self):
+        self.seleae_dev.set_capture_seconds(300) # test for maximum 5 minutes
+        self.execution_start_time = time.time()
 
     def on_execute(self):
         self.seleae_dev.capture_start()
 
     def on_terminate(self):
         self.seleae_dev.capture_stop()
-        self.seleae_dev.export_data2('/tmp/logic.csv')
-        shutil.copy('/tmp/logic.csv', self.output_waveform_path)
+        self.seleae_dev.export_data2(LogicSaleaeWrapper.TMP_OUTPUT_PATH)
+
+        execution_time = time.time() - self.execution_start_time
+        
+        writer = LogicSaleaeWaveformFileWriter(self.output_waveform_path)
+        writer.set_period_sec(execution_time)
+        
+        for pin_set in self.config['output_metadata']['pins']:
+            writer.add_display_param(
+                    plot_name=pin_set['label'],
+                    plot_pins=pin_set['indexes'],
+            )
+        
+        writer.set_raw_data_path(LogicSaleaeWrapper.TMP_OUTPUT_PATH)
+        writer.marshal()
     
     def on_reset_after_execution(self):
         pass
