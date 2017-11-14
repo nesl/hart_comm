5c086785b8e2a82d7a38bb2a482f223e86fafe27
TimeString <timestringalpha@gmail.com>
2017-11-14 00:38:21 -0800

    [fall17-hw2] Update prepare.sh

--- testenv/prepare.sh
+++ testenv/prepare.sh
@@ -5,12 +5,26 @@ kill -9 `ps aux | grep student_bin | awk '{print $2}'`
 cd "$(dirname "$0")"
 rm files/*
 
-# mount module
+# load module
 if [ ! -c /dev/ptp1 ]; then
     capes BBB-AM335X
     echo 'no'
 fi
 
+# config-pin
 config-pin overlay cape-universala
 config-pin P8_8 timer
 config-pin P8_10 timer
+
+# flush the queue timestamps
+testptp -d /dev/ptp1 -i 1 -L 1,1 >/dev/null 2>&1
+testptp -d /dev/ptp1 -i 1 -e 100 >/dev/null 2>&1 &
+
+PID=$!
+echo PTP flushing process launched at PID $PID.
+
+sleep 1
+
+kill -TERM $PID
+
+echo "PTP queue flushed."
